0146af912d10582a3057b82fe2fc342a4069e5a6
gohan007 <32301387+gohan007@users.noreply.github.com>
2017-10-06 02:23:33 +0530

    Add files via upload

---
+++ Ide.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+#include<conio.h>
+int main()
+{
+	int i,n A[1000];
+	for(i=0;i<1000;i++)
+	{ if(i==0)
+	    scanf("%d ",&A[i]); 
+	    else 
+	    scanf(" %d",&A[i]);
+	}
+	for(j=0;j<1000;j++)
+	{
+	    printf("%d ",A[j]);
+	}
+	getch();
+	return 0;
+}
